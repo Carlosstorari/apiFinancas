e2c0db814a47639536a112d2ed5293a9886dab7f
Carlos <carlos.storari95@gmail.com>
2021-03-08 20:54:57 -0300

    change class name

--- src/main/java/com/example/apiFinancas/controller/TransacaoController.java
+++ src/main/java/com/example/apiFinancas/controller/TransacaoController.java
@@ -10,7 +10,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/transacao")
-public class ClienteController {
+public class TransacaoController {
 
     @Autowired
     private TransacaoRepository transacaoRepository;
